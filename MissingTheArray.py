17b634da34d67a7e303a14c2c767590fc16df642
mohamednabil1406 <connectwithmhd@gmail.com>
2025-08-06 20:26:13 +0530

    New

---
+++ MissingTheArray.py
@@ -0,0 +1,16 @@
+def find_missing_number(arr):
+    n = len(arr) + 1  # Total numbers from 1 to n
+    expected_sum = 0
+    for i in range(1, n + 1):
+        expected_sum += i
+
+    actual_sum = 0
+    for i in range(len(arr)):
+        actual_sum += arr[i]
+
+    missing_number = expected_sum - actual_sum
+    return missing_number
+
+# Example input
+arr = [3, 7, 1, 2, 8, 4, 5]
+print("Missing number is:", find_missing_number(arr))
